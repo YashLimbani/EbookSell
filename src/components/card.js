d83511f2fe8e6ea5ae914784dda3d1ac0a39ce3d
Yash Limbani <yashlimbani2003@gmail.com>
2023-08-01 15:00:33 +0530

    used material UI THEME

--- src/components/card.js
+++ src/components/card.js
@@ -5,8 +5,9 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 275,
     backgroundColor: "grey",
@@ -22,64 +23,35 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-});
+}));
 
 export default function SimpleCard() {
   const classes = useStyles();
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        justifyItems: "center",
-        alignItems: "center",
-      }}
-    >
-      <Card
-        variant="outlined"
-        className={classes.root}
-        style={{
-          width: "50%",
-        }}
-      >
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            <span
-              style={{
-                color: "red",
-              }}
-            >
-              USER{" : "}
-            </span>
-          </Typography>
-          <Typography variant="body2" component="h2">
-            Y.S Limbani <br />
-          </Typography>
-          <Typography className={classes.title} color="textSecondary">
-            <span
-              style={{
-                color: "red",
-              }}
-            >
-              COLLEGE{" : "}
-            </span>
-          </Typography>
-          <Typography variant="body2" component="p">
-            GEC, Bhavnagar
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" variant="contained" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
-    </div>
+    <Grid container alignItems="center" justifyContent="center">
+      <Grid item>
+        <Card variant="outlined" className={classes.root} align="center">
+          <CardContent>
+            <Typography className={classes.title} color="primary" gutterBottom>
+              USER :
+            </Typography>
+            <Typography variant="h5" component="h2" color="secondary">
+              Y.S Limbani
+            </Typography>
+            <Typography className={classes.pos} color="primary">
+              COLLEGE:
+            </Typography>
+            <Typography variant="body2" component="p" color="secondary">
+              From Gec,Bvn
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
